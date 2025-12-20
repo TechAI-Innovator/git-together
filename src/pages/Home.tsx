5b61821cd288b20c43aba529c976ef022d438388
Ayomide Oluwanifesimi Adu <aduayomide5@gmail.com>
2025-12-20 18:22:37 +0100

    Added all assets for the home page and started working on the home page, creating the containers as it should be followed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,71 @@
 const Home: React.FC = () => {
   return (
     <div className="w-full min-h-screen bg-background font-[var(--font-poppins)]">
-      {/* Blank home page */}
+      {/* 1st section */}
+      <div>
+        <div>
+
+        </div>
+        <div>
+          
+        </div>
+      </div>
+
+      {/* 2nd section */}
+      <div>
+        
+      </div>
+
+      {/* third section */}
+      <div>
+        <div>
+
+        </div>
+        <div>
+          
+        </div>
+      </div>
+
+      {/* 4th section */}
+      <div>
+        <div>
+          {/* 1st card */}
+          <div>
+
+          </div>
+          {/* 2nd card */}
+          <div>
+
+          </div>
+        </div>
+
+        <div>
+          {/* 3rd card just 20% showing, rest overflowing */}
+          <div>
+
+          </div>
+          {/* 4th card */}
+          <div>
+
+          </div>
+        </div>
+      </div>
+
+      {/* 5th section */}
+      <div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </div>
     </div>
   );
 };
